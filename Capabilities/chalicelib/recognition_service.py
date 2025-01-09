14b7d72dcbe13b23997e35822fa5de53bf61c669
Emander Dangla <emanderdangla@Emanders-Laptop.local>
2025-01-09 15:54:51 -0500

    add deployment to aws

--- Capabilities/chalicelib/recognition_service.py
+++ Capabilities/chalicelib/recognition_service.py
@@ -1,5 +1,8 @@
 import boto3
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 class RecognitionService:
     def __init__(self, storage_service):
@@ -7,14 +10,20 @@ class RecognitionService:
         self.bucket_name = storage_service.get_storage_location()
 
     def detect_text(self, file_name):
-        response = self.client.detect_text(
-            Image = {
-                'S3Object': {
-                    'Bucket': self.bucket_name,
-                    'Name': file_name
+        logger.info(f"DetectText called with Bucket: {self.bucket_name}, File: {file_name}")
+        try:
+            response = self.client.detect_text(
+                Image={
+                    'S3Object': {
+                        'Bucket': self.bucket_name,
+                        'Name': file_name
+                    }
                 }
-            }
-        )
+            )
+            logger.info(f"DetectText response: {response}")
+        except Exception as e:
+            logger.error(f"Error during DetectText: {e}")
+            raise
 
         lines = []
         for detection in response['TextDetections']:
@@ -26,4 +35,3 @@ class RecognitionService:
                 })
 
         return lines
-
--- Website/scripts.js
+++ Website/scripts.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const serverUrl = "http://35.183.128.71:8000";
+const serverUrl = "http://3.99.21.94:8000";
 
 async function uploadImage() {
     // encode input file as base64 string for upload
